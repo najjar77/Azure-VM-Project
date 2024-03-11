2f50b6480b75a3af8a31d29dab24e5f978df56ab
Amiin Najjar <najjar.amiin@gmail.com>
2024-03-11 11:07:32 +0100

    added Upload Method

--- components/FileUploaderComponent.vue
+++ components/FileUploaderComponent.vue
@@ -1,22 +1,49 @@
-<script setup>
+<script lang="ts" setup>
 import Button from "primevue/button";
 import FileUpload from "primevue/fileupload";
 import Card from "primevue/card";
-
 import "primeicons/primeicons.css";
+import { ref } from "vue";
+
+const files = ref<FileList | null>(null);
+//@ts-ignore
+function handleFileChange(e: Event) {
+  //@ts-ignore
+  files.value = (e.target as HTMLInputElement).files;
+  //console.log(files.value);
+}
+async function handleFileSubmit() {
+  const fd = new FormData();
+  if (files.value) {
+    Array.from(files.value).forEach((file, index) => {
+      fd.append(`index${index}`, file);
+    });
+  }
+  const data = await $fetch("/api/file", {
+    method: "POST",
+    body: fd,
+  });
+  console.log(data);
+}
 </script>
 <template>
-  <Card>
+  <Card class="Card-Custom">
     <template #title>File Uploader</template>
     <template #content>
       <p class="m-0">to test you'r script upload the file here.</p>
     </template>
     <template #footer>
-      <FileUpload mode="basic" url="/api/upload" :maxFileSize="100000000" />
-      <Button label="Use Uploaded File" icon="pi pi-check" />
+      <div>
+        <form @submit.prevent="handleFileSubmit">
+          <input type="file" @change="handleFileChange" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </template>
   </Card>
-
-  <div></div>
 </template>
-<style scoped></style>
+<style scoped>
+.Card-Custom {
+  width: 400px;
+}
+</style>
---
+++ server/api/file.post.ts
@@ -0,0 +1,29 @@
+import path from "path";
+import fs from "fs";
+
+export default defineEventHandler(async (event) => {
+  try {
+    const files = await readMultipartFormData(event);
+    if (!files) {
+      return createError({ statusCode: 400, data: "File is invalid." });
+    }
+
+    const UploadedFilePaths: string[] = [];
+
+    files.forEach((file) => {
+      const filePath = path.join(
+        process.cwd(),
+        "UploadedFiles",
+        file.filename as string
+      );
+      fs.writeFileSync(filePath, file.data);
+      UploadedFilePaths.push(file.filename as string);
+    });
+    return UploadedFilePaths;
+  } catch (error) {
+    return createError({
+      statusCode: 500,
+      data: "Something went wrong with the server!",
+    });
+  }
+});
