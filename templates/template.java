a14c0df67e5b323f6d90aea1891e63fce28dd25e
Amiin Najjar <najjar.amiin@gmail.com>
2024-03-14 17:04:43 +0100

    added template

---
+++ templates/template.java
@@ -0,0 +1,18 @@
+package measurement;
+
+public class Measurement {
+
+    public static void main(String[] args) {
+        try {
+            // Delay execution for 3 seconds
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            // Handle interruption gracefully
+            e.printStackTrace();
+        }
+
+        /** Here comes your code. Please do not modify the code above. :) **/
+
+        /** End **/
+    }
+}
